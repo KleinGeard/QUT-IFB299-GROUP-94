532dea7b7340572c9d3aec75a1d778a71b28ddba
Daniel Field <danielfield3515@gmail.com>
2017-09-03 17:35:22 +1000

    started to add search functionality. Dummy search results displayed as tiles beside map

--- smart_city_app/views.py
+++ smart_city_app/views.py
@@ -5,18 +5,28 @@ from django.http import HttpResponse
 def index(request):
     page_title = 'Smart City - Welcome Page'
     
-    # Get the selected location
-    location = request.GET.get('city')
+    location = request.GET.get('city') # Stored as a url parameter so users can bookmark the url with a specific city
+    q = request.POST.get('query') # Retrieve map search query from text field
+
+    # Only update the session variable if it's not empty
+    if (q != None and q != ''):
+        request.session['query'] = q
 
     # If none of the valid cities, set to Australia
     if (location != 'Brisbane' and location != 'Sydney' and location != 'Perth' and location != 'Hobart'):
-        location = 'Australia'
+        location = 'Australia' # I know it's not a city btw
 
+    # TODO: get results from database and compare with query, then construct list
+    results = {'Queensland University of Technology','University of Queensland', 'Test location', 'A museum somewhere', 'Cinema'}
+
+    # Construct the markup based on the template, and variables
     return render(request, "smart_city_app/index.html",
     {
         # Pass variables into template
         "page_title": page_title,
-        "location": location
+        "location": location,
+        "query": request.session['query'],
+        "results": results,
     })
 
 def about(request):
